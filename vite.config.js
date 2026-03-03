3628e46eb800129f4ad9d0a9cb465b0513ae18a1
seyoung <iam3zero@gmail.com>
2026-03-03 19:42:18 +0900

    fix: 배포 경로 설정 수정

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Lazyz/", // 👈 여기에 본인의 레포지토리 이름을 넣어야 합니다! (슬래시 주의)
 })
